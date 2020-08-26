25774649128c500e34b226fa5c88753c594a4c4c
pratiksuthar1 <pratiksuthar1@gmail.com>
2020-08-26 22:19:53 +0530

    My Django Girls app, first commit

--- payments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import initiate_payment, callback
-
-urlpatterns = [
-    path('pay/', initiate_payment, name='pay'),
-    path('callback/', callback, name='callback'),
-    ]
